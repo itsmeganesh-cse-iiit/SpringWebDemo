2c9f8e433931da4197f9a613924c1ea388d9f7a4
Ganesh Koilada <itsmeganeshcse@gmail.com>
2019-11-26 17:51:47 +0530

    Stage all changes

---
+++ src/main/java/com/ganesh/springweb/Employee.java
@@ -0,0 +1,30 @@
+package com.ganesh.springweb;
+
+public class Employee {
+	private short empId;
+	private String empName;
+	private String language;
+	
+	public short getEmpId() {
+		return empId;
+	}
+	public void setEmpId(short empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	
+	
+
+}
